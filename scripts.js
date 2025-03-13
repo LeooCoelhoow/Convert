e91c4686e675011215c7e314a4addfb6ead0c15e
LeooCoelhoow <leoocoelhoow06@gmail.com>
2025-03-13 20:58:20 -0300

    Criando formatações e exibindo o total

--- scripts.js
+++ scripts.js
@@ -8,6 +8,9 @@ const GBP = 6.08
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
+const result = document.getElementById("result")
 
 // Manipulando o input amount para receber somente números
 amount.addEventListener("input", () => { // para pegar o valor do input
@@ -36,5 +39,33 @@ form.onsubmit = (event) => {
 
 // Função para converter a moeda
 function convertCurrency(amount, price, symbol){
-  console.log(amount, price, symbol)
+  try {
+    // Exibindo a cotação da moeda selecionada
+    description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`
+
+    // Calcula o total
+    let total = amount * price
+
+    // Exibe o total
+    result.textContent = `${total} Reais`
+
+    // Aplica a classe que exibe o footer para mostrar o resultado
+    footer.classList.add("show-result")
+
+  } catch (error){
+    // Remove a classe do footer removendo ele da tela
+    footer.classList.remove("show-result")
+    
+    console.log(error)
+    alert("Não foi possível converter. Tente novamente mais tarde.")
+  }
+}
+
+// Formata a moeda em real brasileiro
+function formatCurrencyBRL(value){
+  // converte para numero para utilizar o toLocaleString
+  return Number(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  })
 }
